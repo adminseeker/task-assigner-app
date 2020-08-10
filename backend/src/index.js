9469909e549d6639c47aa34d02b97ec8ad85e8c9
Aravind A <aravind.arali@gmail.com>
2020-08-10 16:18:33 +0530

    deploy testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const connectToMongoDB = require("./db/mongoose");
 
@@ -13,6 +14,14 @@ app.use("/api/users/",require("./routers/api/users"));
 app.use("/api/auth/",require("./routers/api/auth"));
 app.use("/api/rooms/",require("./routers/api/rooms"));
 app.use("/api/upload/",require("./routers/api/upload"));
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static("../../frontend/build"));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"..","..","frontend","build","index.html"));
+    })
+}
 
 app.get("/",(req,res)=>{
     res.json({"msg":"Welcome"});
