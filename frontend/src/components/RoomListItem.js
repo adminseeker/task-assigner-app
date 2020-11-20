4c52ccda0f89530e432d153e688056c82c899cb5
Aravind A <aravind.arali@gmail.com>
2020-11-21 03:01:00 +0530

    Added card styling to rooms

--- frontend/src/components/RoomListItem.js
+++ frontend/src/components/RoomListItem.js
@@ -5,26 +5,60 @@ import { connect } from "react-redux";
 import axios from "axios";
 import { getRooms } from "../actions/rooms";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  }
+});
+
+
 const RoomListItem = (props)=>{
+    const classes = useStyles();
     const deleteRoom =async ()=>{
         await axios.delete("/api/rooms/"+props.room._id);
     }
     return(
-        <div >
-        <div className="content-container-item rooms-grid-item">
-            <Link  to={"/rooms/"+props.room._id}>
-                <div>
-                    <h3>{props.room.className}</h3>
-                    <p>Created on {moment(props.room.date).format('MMMM Do, YYYY')}</p>
-                    <p>students:{props.room.students.length}</p>
-                </div>
-            </Link><br></br>
-            {props.isTeacher && <button onClick={async (e)=>{await deleteRoom(); await props.dispatch(getRooms());}}>Remove</button>}
-            {!props.isTeacher && <button onClick={async (e)=>{await deleteRoom(); await props.dispatch(getRooms());}}>Leave This Class Room</button>}
-        </div>
-            
-        </div>
-    
+        <Card className={classes.root}>
+        <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={props.room._id}
+          height="120"
+          image="/images/classroom.jpg"
+          title={props.room.className}
+        />
+        <Link  to={"/rooms/"+props.room._id}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" color="primary">
+          {props.room.className}
+          <Typography variant="body" color="textSecondary" component="p">
+            Created on {moment(props.room.date).format('MMMM Do, YYYY')}
+          </Typography>
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h5" color="textPrimary">
+          students:{props.room.students.length}
+          </Typography>
+        </CardContent>
+        </Link>
+      </CardActionArea>
+      <CardActions>
+      {props.isTeacher && <Button size="small" color="secondary" onClick={async (e)=>{await deleteRoom(); await props.dispatch(getRooms());}}>Remove</Button>}
+      {!props.isTeacher && <Button size="small" color="secondary" onClick={async (e)=>{await deleteRoom(); await props.dispatch(getRooms());}}>Leave This Class Room</Button>}
+      </CardActions>
+        </Card>
+        
     )
 };
 
--- frontend/src/components/RoomsList.js
+++ frontend/src/components/RoomsList.js
@@ -4,24 +4,48 @@ import { connect } from "react-redux";
 import LoadingPage from "./LoadingPage";
 import AddRoom from "./AddRoom";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1), //grid padding
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const RoomsList = (props) => {
+    const classes = useStyles();
     return (
         props.loading_rooms ? <LoadingPage /> :
-        <div className="rooms-grid">
+        <div className={classes.root}>
+        <Grid container xs={12} spacing={1} >
             {
                 props.rooms.length === 0 ?(
                     <h3>No Classrooms</h3>
                 ) : (
                     props.rooms.map((room)=>(
+                        
+                        <Grid item xs={12} md={6} lg={3} >
                         <RoomListItem key={room._id} room={room} user={props.user} teacher={props.teacher}/>
+                        </Grid>
+                        
                     ))
+
                 )
+                
             }
             <div>
                 {
                     props.user.isTeacher && <AddRoom />
                 }
             </div>
+            </Grid>
         </div>
     )
 }
@@ -33,3 +57,4 @@ const mapStateToProps= (state)=>({
 })
 
 export default connect(mapStateToProps)(RoomsList);
+
