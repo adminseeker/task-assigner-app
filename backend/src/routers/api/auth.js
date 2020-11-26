8c45b4eea2967fadc6dca583180c4c9f51ee230e
Aravind A <aravind.arali@gmail.com>
2020-11-26 17:37:03 +0530

    Added forgot password APIs to backend

--- backend/src/routers/api/auth.js
+++ backend/src/routers/api/auth.js
@@ -5,6 +5,9 @@ const auth = require("../../middleware/auth");
 const generateAuthToken = require("../../token/generateAuthToken");
 
 const User = require("../../models/User");
+const ForgotPassword = require("../../models/ForgotPassword");
+
+const mailer = require("../../mailer/mailer");
 
 const router = express.Router();
 
@@ -102,5 +105,58 @@ router.post("/logoutAll",auth,async (req,res)=>{
         console.log(error)
     }
 });
+
+/* 
+    route : "/api/rooms/room_id/students/invite",
+    desc : "Teacher Invites Students",
+    auth : "Teacher",
+    method: "POST"
+*/
+
+router.post("/sendotp",async (req,res)=>{
+    try{
+        const email = req.body.email;
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({"msg":"No user exists with that email!"});
+        }
+        let emailHTML = "<h2>You have requested for changing the password. Here is your OTP. Don't share with anyone.</h2>\n <p>Your OTP is:</p> ";
+        let subject = "OTP for changing Tasker Password";
+        let otp = Math.floor(100000 + Math.random() * 900000);
+        const forgotPasswordrequest = new ForgotPassword({email,otp});
+        await forgotPasswordrequest.save();
+        await mailer(email,text="",html=emailHTML+"<h1>"+otp+"</h1>",subject);
+        res.json({"msg":"OTP sent to "+email});
+    } catch (error) {
+        res.status(500).send("Server Error!");
+        console.log(error);
+    }
+});
+
+router.post("/newpassword",async (req,res)=>{
+    try{
+        const email = req.body.email;
+        const otp = req.body.otp;
+        let newPassword = req.body.newPassword;
+        const forgotPasswordrequest = await ForgotPassword.findOne({email})
+        if(!forgotPasswordrequest){
+            return res.json({"msg":"invalid otp!"});
+        }
+        if(otp==forgotPasswordrequest.otp){
+           const salt = await bcrypt.genSalt(10);
+           newPassword = await bcrypt.hash(newPassword,salt);
+           const user = await User.findOneAndUpdate({email},{password:newPassword},{new:true});
+           await ForgotPassword.deleteMany({email});
+           return res.json({"msg":"Password Reset Completed Successfully!"}) 
+        }else{
+            return res.json({"msg":"invalid otp!"});
+        }
+        
+    } catch (error) {
+        res.status(500).send("Server Error!");
+        console.log(error);
+    }
+});
+
 
 module.exports = router;
